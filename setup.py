4664238c2e439c84250ee6180f52e9fd63842fe5
Kevin George <kevingeorge2015@gmail.com>
2024-08-08 22:42:15 -0700

    push update1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='kevin george',
+    author_email='kevingeorge2015@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain_community"],
+    packages=find_packages()
+)
